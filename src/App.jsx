28daaaae09567e7243bf27dbd933707ce0905ee2
Alaa <aayyaser@hotmail.com>
2025-07-19 04:37:10 +0300

    fetching questions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import "./index.css";
 import Header from "./Header";
 import MainSection from "./MainSection";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      throw new Error("Unknown Action");
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
